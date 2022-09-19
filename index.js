6cd4cead323dac49bdb52d8f5b37b6df06efd064
skydodle <skydodle@gmail.com>
2022-09-19 16:30:24 -0700

    login route setup; spotify OAuth init

--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-const express = require("express");
-
+require('dotenv').config();
+const express = require('express');
+const querystring = require('querystring');
+const axios = require('axios');
 const app = express();
+const port = 8888;
 
-// route handler
-app.get("/", (req, res) => {
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.get('/', (req, res) => {
   const data = {
-    name: "Johnny",
+    name: 'Johnny',
     isAwesome: true,
   };
 
   res.json(data);
 });
 
-app.get("/awesome-generator", (req, res) => {
-  const { name, isAwesome } = req.query;
-  res.send(`${name} is ${JSON.parse(isAwesome) ? "really" : "not"} awesome`);
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  let text = '';
+  const possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = 'spotify_auth_state';
+
+app.get('/login', (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scope = 'user-read-private user-read-email';
+
+  const queryParams = querystring.stringify({
+    client_id: CLIENT_ID,
+    response_type: 'code',
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
 });
 
-const port = 8888;
+app.get('/callback', (req, res) => {
+  const code = req.query.code || null;
+  res.send('callback');
+});
 
 app.listen(port, () => {
   console.log(`Express app listening at http://localhost:${port}`);
