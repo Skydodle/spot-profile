3c012d9dbeae32a53ea5c6fb4fa98f8ec69a788a
skydodle <skydodle@gmail.com>
2022-09-19 14:32:37 -0700

    nodemon express init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+// route handler
+app.get("/", (req, res) => {
+  const data = {
+    name: "Johnny",
+    isAwesome: true,
+  };
+
+  res.json(data);
+});
+
+app.get("/awesome-generator", (req, res) => {
+  const { name, isAwesome } = req.query;
+  res.send(`${name} is ${JSON.parse(isAwesome) ? "really" : "not"} awesome`);
+});
+
+const port = 8888;
+
+app.listen(port, () => {
+  console.log(`Express app listening at http://localhost:${port}`);
+});
