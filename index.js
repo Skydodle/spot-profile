3068be080a88330bcfe48d352e6335490961fd07
skydodle <skydodle@gmail.com>
2022-09-19 16:54:36 -0700

    setup callback route to request token after auth request

--- index.js
+++ index.js
@@ -54,7 +54,32 @@ app.get('/login', (req, res) => {
 
 app.get('/callback', (req, res) => {
   const code = req.query.code || null;
-  res.send('callback');
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: REDIRECT_URI,
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(
+        `${CLIENT_ID}:${CLIENT_SECRET}`
+      ).toString('base64')}`,
+    },
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        res.send(`<pre>${JSON.stringify(response.data, null, 2)} </pre>`);
+      } else {
+        res.send(response);
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 });
 
 app.listen(port, () => {
