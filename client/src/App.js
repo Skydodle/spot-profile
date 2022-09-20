071d872b39eca2da141c2f8b47f6e44a50ff6acf
skydodle <skydodle@gmail.com>
2022-09-19 18:51:06 -0700

    retrieve tokens on client side as variables

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    // console.log(accessToken);
+    // console.log(refreshToken);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
