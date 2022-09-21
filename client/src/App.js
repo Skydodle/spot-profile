bcdf3995af581e35a7ad4cffdd0d9a9ff5c864f4
skydodle <skydodle@gmail.com>
2022-09-20 18:31:16 -0700

    Added style for logout button; moved to right

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,25 @@ import {
 } from 'react-router-dom';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
-import { GlobalStyle } from './styles';
 import { Login, Profile } from './pages';
+import { GlobalStyle } from './styles';
+import styled from 'styled-components/macro';
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0, 0, 0, 0.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
 
 // Scroll to top of page when changing routes
 // https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
@@ -48,26 +65,29 @@ function App() {
         {!token ? (
           <Login />
         ) : (
-          <Router>
-            <ScrollToTop />
-            <Switch>
-              <Route path="/top-artists">
-                <h1>Top Artists</h1>
-              </Route>
-              <Route path="/top-tracks">
-                <h1>Top Tracks</h1>
-              </Route>
-              <Route path="/playlists/:id">
-                <h1>Playlist</h1>
-              </Route>
-              <Route path="/playlists">
-                <h1>Playlists</h1>
-              </Route>
-              <Route path="/">
-                <Profile />
-              </Route>
-            </Switch>
-          </Router>
+          <>
+            <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>
+            <Router>
+              <ScrollToTop />
+              <Switch>
+                <Route path="/top-artists">
+                  <h1>Top Artists</h1>
+                </Route>
+                <Route path="/top-tracks">
+                  <h1>Top Tracks</h1>
+                </Route>
+                <Route path="/playlists/:id">
+                  <h1>Playlist</h1>
+                </Route>
+                <Route path="/playlists">
+                  <h1>Playlists</h1>
+                </Route>
+                <Route path="/">
+                  <Profile />
+                </Route>
+              </Switch>
+            </Router>
+          </>
         )}
       </header>
     </div>
