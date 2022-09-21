473be61148dc4d6e2294e706920531b5f0372468
skydodle <skydodle@gmail.com>
2022-09-20 22:07:16 -0700

    Refactored range buttons to resuable component

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { catchErrors } from '../utils';
 import { getTopArtists } from '../spotify';
-import { ArtistsGrid, SectionWrapper } from '../components';
+import { ArtistsGrid, SectionWrapper, TimeRangeButtons } from '../components';
 
 const TopArtists = () => {
   const [topArtists, setTopArtists] = useState(null);
@@ -20,32 +20,10 @@ const TopArtists = () => {
 
   return (
     <main>
-      <ul>
-        <li>
-          <button
-            className={activeRange === 'short' ? 'active' : ''}
-            onClick={() => setActiveRange('short')}
-          >
-            This Month
-          </button>
-        </li>
-        <li>
-          <button
-            className={activeRange === 'medium' ? 'active' : ''}
-            onClick={() => setActiveRange('medium')}
-          >
-            Last 6 Months
-          </button>
-        </li>
-        <li>
-          <button
-            className={activeRange === 'long' ? 'active' : ''}
-            onClick={() => setActiveRange('long')}
-          >
-            All time
-          </button>
-        </li>
-      </ul>
+      <TimeRangeButtons
+        activeRange={activeRange}
+        setActiveRange={setActiveRange}
+      />
       {topArtists && (
         <SectionWrapper title="Top Artists" breadcrumb="true">
           <ArtistsGrid artists={topArtists.items.slice(0, 10)} />
