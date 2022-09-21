163b6d7fb7edcd114666059b30cd72e9427c9985
skydodle <skydodle@gmail.com>
2022-09-21 16:06:00 -0700

    Extract nested track object into tracks state to supply the TrackList component for displaying playlist tracks

--- client/src/pages/Playlist.js
+++ client/src/pages/Playlist.js
@@ -2,25 +2,70 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { catchErrors } from '../utils';
 import { getPlaylistById } from '../spotify';
+import { StyledHeader } from '../styles';
+import { TrackList, SectionWrapper } from '../components';
 
 const Playlist = () => {
   const { id } = useParams();
   const [playlist, setPlaylist] = useState(null);
+  const [tracks, setTracks] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const currentPlaylistData = await getPlaylistById(id);
-      setPlaylist(currentPlaylistData.data);
+      const { data } = await getPlaylistById(id);
+      setPlaylist(data);
 
-      console.log(currentPlaylistData.data);
+      const mapTracks = data.tracks.items.map(({ track }) => track);
+      setTracks(mapTracks);
+
+      // console.log(mapTracks);
     };
     catchErrors(fetchData());
   }, [id]);
 
+  // console.log(playlist);
+
   return (
-    <main>
-      <h1>Individual Playlist Page</h1>
-    </main>
+    <>
+      {playlist && (
+        <>
+          <StyledHeader>
+            <div className="header__inner">
+              {playlist.images.length && playlist.images[0].url && (
+                <img
+                  className="header__img"
+                  src={playlist.images[0].url}
+                  alt="Playlist Artwork"
+                />
+              )}
+              <div>
+                <div className="header__overline">Playlist</div>
+                <h1 className="header__name">{playlist.name}</h1>
+                <p className="header__meta">
+                  {playlist.followers.total ? (
+                    <span>
+                      {playlist.followers.total}{' '}
+                      {`follower${playlist.followers.total !== 1 ? 's' : ''}`}
+                    </span>
+                  ) : null}
+                  <span>
+                    {playlist.tracks.total}{' '}
+                    {`song${playlist.tracks.total !== 1 ? 's' : ''}`}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </StyledHeader>
+
+          <main>
+            <SectionWrapper title="Playlist tracks" breadcrumb="true">
+              {playlist && tracks && <TrackList tracks={tracks} />}
+            </SectionWrapper>
+          </main>
+        </>
+      )}
+    </>
   );
 };
+
 export default Playlist;
